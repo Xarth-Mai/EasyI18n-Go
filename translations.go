f971eab98021681d5d265ccc25a5529e90bc17b9
Xarthleo <xarthleo@gmail.com>
2024-08-03 05:12:09 +0800

    init

---
+++ translations.go
@@ -0,0 +1,16 @@
+package main
+
+var translations = map[string]map[string]string{
+	"en": {
+		"greeting": "Hello, %s!",
+	},
+	"zhs": {
+		"greeting": "你好，%s!",
+	},
+	"zht": {
+		"greeting": "你好，%s!",
+	},
+	"jp": {
+		"greeting": "こんにちは、%s!",
+	},
+}
