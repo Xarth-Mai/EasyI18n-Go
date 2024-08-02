41a774d66d6cda40da699768dac53e7c639aa1e6
Xarthleo <xarthleo@gmail.com>
2024-08-03 05:51:40 +0800

    init

--- example_main.go
+++ example_main.go
@@ -2,44 +2,17 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/Xarth-Mai/EasyI18n-Go/translations"
 )
 
-var EasyI18n_translations = map[string]map[string]string{
-	"en": {
-		"greeting": "Hello, %s!",
-		"farewell": "Goodbye, %s!",
-	},
-	"zhs": {
-		"greeting": "你好, %s!",
-		"farewell": "再见, %s!",
-	},
-	"zht": {
-		"greeting": "你好, %s!",
-		"farewell": "再見, %s!",
-	},
-	"jp": {
-		"greeting": "こんにちは、%s!",
-		"farewell": "さようなら、%s!",
-	},
-	"fr": {
-		"greeting": "Bonjour, %s!",
-		"farewell": "Au revoir, %s!",
-	},
-	"es": {
-		"greeting": "Hola, %s!",
-		"farewell": "Adiós, %s!",
-	},
-	"de": {
-		"greeting": "Hallo, %s!",
-		"farewell": "Auf Wiedersehen, %s!",
-	},
-}
-
 func main() {
-	// 传入自定义的翻译数据
-	translations.SetCustomTranslations(EasyI18n_translations)
+	// 从文件加载自定义的翻译数据
+	err := translations.LoadTranslationsFromFile("translations.json")
+	if err != nil {
+		log.Fatalf("Error loading translations: %v", err)
+	}
 
 	translations.InitLanguage()
 	fmt.Println(translations.Translate("greeting", "World"))
--- translations/translations.go
+++ translations/translations.go
@@ -1,6 +1,7 @@
 package translations
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 	"strings"
@@ -10,8 +11,21 @@ var EasyI18nTranslations = map[string]map[string]string{}
 
 var currentLanguage string
 
-func SetCustomTranslations(data map[string]map[string]string) {
+func LoadTranslationsFromFile(filename string) error {
+	file, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	decoder := json.NewDecoder(file)
+	var data map[string]map[string]string
+	if err := decoder.Decode(&data); err != nil {
+		return err
+	}
+
 	EasyI18nTranslations = data
+	return nil
 }
 
 func InitLanguage() {
